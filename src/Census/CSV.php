f5a73e502db5d937a9a152f329cb745717c92862
Austen McDonald <austen.mcdonald@gmail.com>
2017-03-03 08:51:01 +0000

    Reflow tabs in CSV.php

--- src/Census/CSV.php
+++ src/Census/CSV.php
@@ -3,71 +3,71 @@
 namespace LotGD\Census;
 
 class CSV {
-  private $logger;
-  private $data;
-  private $filename;
+    private $logger;
+    private $data;
+    private $filename;
 
-  public function __construct(\Monolog\Logger $logger, string $filename, string $contents) {
-    $this->logger = $logger;
-    $this->filename = $filename;
-    $lines = explode(PHP_EOL, $contents);
-    $this->data = array_map('str_getcsv', $lines);
+    public function __construct(\Monolog\Logger $logger, string $filename, string $contents) {
+        $this->logger = $logger;
+        $this->filename = $filename;
+        $lines = explode(PHP_EOL, $contents);
+        $this->data = array_map('str_getcsv', $lines);
 
-    $end = count($this->data) - 1;
-    if ($end >= 0 && count($this->data[$end]) == 1 && $this->data[$end][0] == null) {
-      array_pop($this->data);
-    }
-  }
-
-  public function contents() : string {
-    $contents = '';
-    foreach ($this->data as $row) {
-      $contents .= implode(",", $row) . "\n";
-    }
-    return $contents;
-  }
-
-  public function appendToRows(array $map) {
-    $csv = $this->filename;
-    $date = date("Y-m-d");
-
-    // Process header...
-    $i = 0;
-    if (count($this->data) == 0) {
-      $this->logger->addError("Couldn't find header row in {$csv}.");
-      return;
-    }
-    $row = $this->data[$i];
-    $columns = count($row);
-    array_push($row, $date);
-    $this->data[$i] = $row;
-    $i++;
-
-    for (; $i < count($this->data); $i++) {
-      $row = $this->data[$i];
-      if ($columns != count($row)) {
-        $this->logger->addError("Row in {$csv} doesn't have the right number of columns.");
-        return;
-      }
-      $s = $row[0];
-      if (array_key_exists($s, $map)) {
-        $value = $map[$s];
-        array_push($row, $value);
-        unset($map[$s]);
-      } else {
-        array_push($row, '');
-      }
-      $this->data[$i] = $row;
+        $end = count($this->data) - 1;
+        if ($end >= 0 && count($this->data[$end]) == 1 && $this->data[$end][0] == null) {
+            array_pop($this->data);
+        }
     }
 
-    foreach ($map as $s => $value) {
-      $row = array($s);
-      for ($j = 0; $j < $columns - 1; $j++) {
-        array_push($row, '');
-      }
-      array_push($row, $value);
-      $this->data[$i] = $row;
-      $i++;
+    public function contents() : string {
+        $contents = '';
+        foreach ($this->data as $row) {
+            $contents .= implode(",", $row) . "\n";
+        }
+        return $contents;
     }
-  }
+
+    public function appendToRows(array $map) {
+        $csv = $this->filename;
+        $date = date("Y-m-d");
+
+        // Process header...
+        $i = 0;
+        if (count($this->data) == 0) {
+            $this->logger->addError("Couldn't find header row in {$csv}.");
+            return;
+        }
+        $row = $this->data[$i];
+        $columns = count($row);
+        array_push($row, $date);
+        $this->data[$i] = $row;
+        $i++;
+
+        for (; $i < count($this->data); $i++) {
+            $row = $this->data[$i];
+            if ($columns != count($row)) {
+                $this->logger->addError("Row in {$csv} doesn't have the right number of columns.");
+                return;
+            }
+            $s = $row[0];
+            if (array_key_exists($s, $map)) {
+                $value = $map[$s];
+                array_push($row, $value);
+                unset($map[$s]);
+            } else {
+                array_push($row, '');
+            }
+            $this->data[$i] = $row;
+        }
+
+        foreach ($map as $s => $value) {
+            $row = array($s);
+            for ($j = 0; $j < $columns - 1; $j++) {
+                array_push($row, '');
+            }
+            array_push($row, $value);
+            $this->data[$i] = $row;
+            $i++;
+        }
+    }
 }
