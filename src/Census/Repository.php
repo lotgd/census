81c8cb5639055927db81db23b882a4b274020315
Austen McDonald <austen.mcdonald@gmail.com>
2017-03-03 08:51:12 +0000

    Reflow tabs in Repository.php

--- src/Census/Repository.php
+++ src/Census/Repository.php
@@ -3,58 +3,58 @@
 namespace LotGD\Census;
 
 class Repository {
-  private $logger;
-  private $client;
+    private $logger;
+    private $client;
 
-  private $user = 'lotgd';
-  private $repo = 'census';
-  private $branch = 'master';
-  private $committer = array(
-    'name' => 'Census Bot',
-    'email' => 'austen.mcdonald@gmail.com');
+    private $user = 'lotgd';
+    private $repo = 'census';
+    private $branch = 'master';
+    private $committer = array(
+        'name' => 'Census Bot',
+        'email' => 'austen.mcdonald@gmail.com');
 
-  public function __construct(\Monolog\Logger $logger) {
-    $this->logger = $logger;
-    $this->client = new \Github\Client();
-    $this->client->authenticate($_ENV['GITHUB_TOKEN'], null, \Github\Client::AUTH_HTTP_TOKEN);
-  }
-
-  public function getFile(string $path) : string {
-    return $this->client->api('repo')->contents()->download($this->user, $this->repo, $path);
-  }
-
-  public function updateFile(string $path, string $content, string $commitMessage) {
-    $oldFile = $this->client->api('repo')->contents()->show($this->user, $this->repo, $path, $this->branch);
-    $fileInfo = $this->client->api('repo')->contents()->update($this->user, $this->repo, $path, $content, $commitMessage, $oldFile['sha'], $this->branch, $this->committer);
-  }
-
-  public function getSites() : array {
-    $sites = array();
-
-    $sitesFile = null;
-    try {
-      $sitesFile = $this->getFile('sites');
-    } catch (Exception $e) {
-      $this->logger->addError("Cannot find sites file.");
-      exit(1);
+    public function __construct(\Monolog\Logger $logger) {
+        $this->logger = $logger;
+        $this->client = new \Github\Client();
+        $this->client->authenticate($_ENV['GITHUB_TOKEN'], null, \Github\Client::AUTH_HTTP_TOKEN);
     }
 
-    if (!$sitesFile) {
-      $this->logger->addError("Could not read from sites file.");
-      exit(1);
+    public function getFile(string $path) : string {
+        return $this->client->api('repo')->contents()->download($this->user, $this->repo, $path);
     }
 
-    $fp = fopen("php://memory", 'r+');
-    fputs($fp, $sitesFile);
-    rewind($fp);
-    while ($line = fgets($fp)) {
-      $line = trim($line);
-      if (strlen($line) > 0) {
-        $sites->add(trim($line));
-      }
+    public function updateFile(string $path, string $content, string $commitMessage) {
+        $oldFile = $this->client->api('repo')->contents()->show($this->user, $this->repo, $path, $this->branch);
+        $fileInfo = $this->client->api('repo')->contents()->update($this->user, $this->repo, $path, $content, $commitMessage, $oldFile['sha'], $this->branch, $this->committer);
     }
-    fclose($fp);
 
-    return $sites;
-  }
+    public function getSites() : array {
+        $sites = array();
+
+        $sitesFile = null;
+        try {
+            $sitesFile = $this->getFile('sites');
+        } catch (Exception $e) {
+            $this->logger->addError("Cannot find sites file.");
+            exit(1);
+        }
+
+        if (!$sitesFile) {
+            $this->logger->addError("Could not read from sites file.");
+            exit(1);
+        }
+
+        $fp = fopen("php://memory", 'r+');
+        fputs($fp, $sitesFile);
+        rewind($fp);
+        while ($line = fgets($fp)) {
+            $line = trim($line);
+            if (strlen($line) > 0) {
+                $sites->add(trim($line));
+            }
+        }
+        fclose($fp);
+
+        return $sites;
+    }
 }
