81290b7cd70d8ab88f70ef2df6d0c0e70294575a
Austen McDonald <austen.mcdonald@gmail.com>
2017-03-03 08:51:24 +0000

    Reflow tabs in Stats.php

--- src/Census/Stats.php
+++ src/Census/Stats.php
@@ -3,27 +3,27 @@
 namespace LotGD\Census;
 
 class Stats {
-  public $count = 0;
-  public $dau = 0;
-  public $mau = 0;
+    public $count = 0;
+    public $dau = 0;
+    public $mau = 0;
 
-  public function add(Stats $s) {
-    $this->count += $s->count;
-    $this->dau += $s->dau;
-    $this->mau += $s->mau;
-  }
-
-  public static function compare(Stats $a, Stats $b): int {
-    return $b->count - $a->count;
-  }
-
-  public static function isEqual(?Stats $a, ?Stats $b): bool {
-    if ($a === null || $b === null) {
-      return false;
+    public function add(Stats $s) {
+        $this->count += $s->count;
+        $this->dau += $s->dau;
+        $this->mau += $s->mau;
     }
-    return
-      $a->count == $b->count &&
-      $a->dau == $b->dau &&
-      $a->mau == $b->mau;
-  }
+
+    public static function compare(Stats $a, Stats $b): int {
+        return $b->count - $a->count;
+    }
+
+    public static function isEqual(?Stats $a, ?Stats $b): bool {
+        if ($a === null || $b === null) {
+            return false;
+        }
+        return
+            $a->count == $b->count &&
+            $a->dau == $b->dau &&
+            $a->mau == $b->mau;
+    }
 }
