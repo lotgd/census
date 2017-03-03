8f642b5885a963881643e3de14588a82db50ebe9
Austen McDonald <austen.mcdonald@gmail.com>
2017-03-03 09:15:30 +0000

    Fix typo in comment in census.php

--- census.php
+++ census.php
@@ -5,7 +5,7 @@ require './vendor/autoload.php';
 // Only report errors.
 error_reporting(E_ERROR | E_PARSE | E_RECOVERABLE_ERROR);
 
-// Only run this every 3 days.
+// Only run this every 2 days.
 if (date('z') % 2 === 0) {
     LotGD\Census\Census::main($argv, $argc);
 }
