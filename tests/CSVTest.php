1f88083086e6786b722133200528666a2dd70374
Austen McDonald <austen.mcdonald@gmail.com>
2017-03-03 08:51:43 +0000

    Reflow tabs in CSVTest.php

--- tests/CSVTest.php
+++ tests/CSVTest.php
@@ -6,43 +6,43 @@ use Monolog\Logger;
 use Monolog\Handler\NullHandler;
 
 class CSVTest extends \PHPUnit_Framework_TestCase {
-  private $logger;
+    private $logger;
 
-  public function __construct() {
-    parent::__construct();
+    public function __construct() {
+        parent::__construct();
 
-    $this->logger = new Logger('test');
-  }
+        $this->logger = new Logger('test');
+    }
 
-  public function setUp() {
-    parent::setUp();
+    public function setUp() {
+        parent::setUp();
 
-    $this->logger = new Logger('test');
-    $this->logger->pushHandler(new NullHandler());
-  }
+        $this->logger = new Logger('test');
+        $this->logger->pushHandler(new NullHandler());
+    }
 
-  public function testReadBack() {
-    $contents = "";
-    $csv = new CSV($this->logger, 'filename', $contents);
-    $this->assertEquals($contents, $csv->contents());
+    public function testReadBack() {
+        $contents = "";
+        $csv = new CSV($this->logger, 'filename', $contents);
+        $this->assertEquals($contents, $csv->contents());
 
-    $contents = "a,b\nc,d\n";
-    $csv = new CSV($this->logger, 'filename', $contents);
-    $this->assertEquals($contents, $csv->contents());
-  }
+        $contents = "a,b\nc,d\n";
+        $csv = new CSV($this->logger, 'filename', $contents);
+        $this->assertEquals($contents, $csv->contents());
+    }
 
-  public function testAppendToRows() {
-    $contents = "a,b\ns1,d\n";
-    $date = date("Y-m-d");
-    $expectedContents = "a,b,{$date}\ns1,d,1\ns2,,2\n";
+    public function testAppendToRows() {
+        $contents = "a,b\ns1,d\n";
+        $date = date("Y-m-d");
+        $expectedContents = "a,b,{$date}\ns1,d,1\ns2,,2\n";
 
-    $csv = new CSV($this->logger, 'filename', $contents);
+        $csv = new CSV($this->logger, 'filename', $contents);
 
-    $csv->appendToRows(array(
-      's1' => 1,
-      's2' => 2,
-    ));
+        $csv->appendToRows(array(
+            's1' => 1,
+            's2' => 2,
+        ));
 
-    $this->assertEquals($expectedContents, $csv->contents());
-  }
+        $this->assertEquals($expectedContents, $csv->contents());
+    }
 }
