a392f4ea20d99afd7a74905b5080036465c380ce
Austen McDonald <austen.mcdonald@gmail.com>
2017-03-03 08:52:31 +0000

    Change to every other day data collection

--- census.php
+++ census.php
@@ -6,6 +6,6 @@ require './vendor/autoload.php';
 error_reporting(E_ERROR | E_PARSE | E_RECOVERABLE_ERROR);
 
 // Only run this every 3 days.
-if (date('z') % 3 === 0) {
+if (date('z') % 2 === 0) {
     LotGD\Census\Census::main($argv, $argc);
 }
